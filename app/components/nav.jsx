6557332c3ded9b9224eb8e624f4cb81f5580ff9b
Martin Aarhus Gregersen <aarhus@dinero.dk>
2017-11-14 21:26:49 +0100

    Initializes repo

---
+++ app/components/nav.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+    return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">Gregersens React Timer</li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>CountDown</IndexLink>
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="http://mgregersen.dk" target="_blank">Martin Gregersen</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    );
+  };
+
+module.exports = Nav;
