d82a9fa2c761025ad0b68dc486b8e112f02d8b78
Martin Aarhus Gregersen <aarhus@dinero.dk>
2017-11-14 21:56:01 +0100

    Adds testsuite (with karma) to app

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,10 @@
+var expect = require('expect');
+
+
+
+describe('App', () => {
+  // Pseudo test to make sure test environment works
+  it('should properly run tests', () => {
+    expect(1).toBe(1);
+  });
+});
